5e4f9fd0c4ba9cebe514da314c6ab0bb60a399c0
paulzzh <28055560+paulzzh@users.noreply.github.com>
2022-11-16 07:39:27 +0800

    1.0.0

--- src/main/java/org/teacon/xibao/Xibao.java
+++ src/main/java/org/teacon/xibao/Xibao.java
@@ -1,20 +1,9 @@
 package org.teacon.xibao;
 
 import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.SidedProxy;
-import cpw.mods.fml.common.event.*;
 import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.common.MinecraftForge;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 @Mod(modid = Tags.MODID, version = Tags.VERSION, name = Tags.MODNAME, acceptedMinecraftVersions = "[1.7.10]")
 public class Xibao {
     public static final ResourceLocation LOCATION = new ResourceLocation("xibao", "textures/xibao.png");
-
-    @Mod.EventHandler
-    // postInit "Handle interaction with other mods, complete your setup based on this."
-    public void postInit(FMLPostInitializationEvent event) {
-
-    }
 }
